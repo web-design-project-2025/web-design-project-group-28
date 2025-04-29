1f035aa733c554e723a979495f60292c61d1a4e5
eridenius <eridenius@gmail.com>
2025-04-29 22:24:47 +0200

    removed data loading in the example api loader

--- src/api_loader.js
+++ src/api_loader.js
@@ -1,12 +1,24 @@
+let userData = [];
+
+async function loadUserData() {
+  try {
+    const response = await JSON.parse(localStorage.getItem("data"));
+    userData = response;
+    console.log("fungerar", userData);
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
 let data = [];
 
 async function loadMuscle() {
   const response = await fetch("https://wger.de/api/v2/muscle/");
   const exerciseData = await response.json();
-  console.log(exerciseData);
-  const foundMuscle = exerciseData.results.find((e) => e.name_en === "Biceps");
 
-  console.log(foundMuscle);
+  const foundMuscle = exerciseData.results.find((e) => e.id === 2);
+
+  console.log(exerciseData);
 }
 
 loadMuscle();
