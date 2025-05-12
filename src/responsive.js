f04c3001a95da56bddd0e6154387281cddad8ad0
eridenius <eridenius@gmail.com>
2025-05-12 17:48:20 +0200

    fixed a bug were the arrows had the same a tag, and fixed responsivness on the slider pages

--- src/responsive.js
+++ src/responsive.js
@@ -1,23 +1,55 @@
 const down = document.getElementById("isResponsive");
 const up = document.getElementById("arrows");
+const currentPage = window.location.pathname;
 const isMobile = window.innerWidth < 600;
 
-if (isMobile) {
-  down.innerHTML = `<a href="index.html">
-          <img class="left" src="img/arrow_left.svg" alt=""
-        /></a>
-        <a href="weight.html">
-          <img class="right" src="img/arrow_right.svg" alt=""
-        /></a>`;
-
-  up.innerHTML = "";
-} else {
-  up.innerHTML = `<a href="index.html">
+let arrowhtml = `<a href="index.html">
       <img class="left" src="img/arrow_left.svg" alt=""
     /></a>
     <a href="weight.html">
       <img class="right" src="img/arrow_right.svg" alt=""
     /></a>`;
+
+function checkPage() {
+  // checks what page the user is on and changes the arrows anchor tags
+  if (currentPage === "/weight.html") {
+    arrowhtml = `<a href="gender.html">
+    <img class="left" src="img/arrow_left.svg" alt=""
+  /></a>
+  <a href="height.html">
+    <img class="right" src="img/arrow_right.svg" alt=""
+  /></a>`;
+  }
+
+  if (currentPage === "/height.html") {
+    arrowhtml = `<a href="weight.html">
+      <img class="left" src="img/arrow_left.svg" alt=""
+    /></a>
+    <a href="age.html">
+      <img class="right" src="img/arrow_right.svg" alt=""
+    /></a>`;
+  }
+
+  if (currentPage === "/age.html") {
+    arrowhtml = `<a href="height.html">
+        <img class="left" src="img/arrow_left.svg" alt=""
+      /></a>
+      <a href="part.html">
+        <img class="right" src="img/arrow_right.svg" alt=""
+      /></a>`;
+  }
+}
+
+checkPage();
+
+//changes the arrows location if the screen is small enough
+
+if (isMobile) {
+  down.innerHTML = arrowhtml;
+
+  up.innerHTML = "";
+} else {
+  up.innerHTML = arrowhtml;
 
   down.innerHTML = "";
 }
