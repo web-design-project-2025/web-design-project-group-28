dd7aed02438cbc8bda0f39e5a87ba2318b3dfc88
eridenius <eridenius@gmail.com>
2025-05-14 21:06:32 +0200

    added a third condetion for the get-started button

--- src/to_gender_checker.js
+++ src/to_gender_checker.js
@@ -1,14 +1,20 @@
 const toGender = document.getElementById("toGender");
 const password = localStorage.getItem("password");
+const data = localStorage.getItem("data");
 
 //checks if the user has a password stored in localstorage and changes the get started button to navigate to signup if they don't
 
 if (password) {
   toGender.innerHTML = `<a href="gender.html">
-      <button class="start-button">Get Started</button></a
+      <button class="start-button">Contniue</button></a
       >`;
 } else {
   toGender.innerHTML = `<a href="signup.html">
       <button class="start-button">Get Started</button></a
+    >`;
+} //if the user has their data and account it redirects to workoutplans
+if (data && password) {
+  toGender.innerHTML = `<a href="workout-plans.html">
+      <button class="start-button">work</button></a
     >`;
 }
